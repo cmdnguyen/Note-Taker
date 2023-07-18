e67c868ca2e521f5f7e02a933c510a7ace565537
cmdnguyen <cmdnguyen94@gmail.com>
2023-07-17 21:31:31 -0500

    Added starter code

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+
+const app = express()
+const PORT = 3001
+
+app.use(express.json());
+app.use(express.urlencoded())
+app.use(express.static('public'))
+
+app,get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app,get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(
+        `application succesfully listening to http://localhost:${PORT}`
+        );
+})
+    .catch((err)=>{
+        console.log(err)
+})
