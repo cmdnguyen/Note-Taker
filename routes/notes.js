cedd6e6ea64e135fd4bd1a1f0dce327cb2268a01
cmdnguyen <cmdnguyen94@gmail.com>
2023-07-19 16:02:34 -0500

    Routed the functionality of notes into notes.js, finished DELETE requests and started on README

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,56 @@
 const notes = require('express').Router()
+const fs = require("fs");
+const uuid = require('../helpers/uuid')
+
+//reads the current data and parses it
+notes.get("/", (req, res) => {
+    let rawdata = fs.readFileSync('./db/db.json');
+    let currentNotes = JSON.parse(rawdata);
+    res.json(currentNotes)});
+
+//add new data
+notes.post("/", (req, res) => {
+  console.info(`${req.method} request received`);
+  //logs the new data
+  console.log(req.body)
+  if (!req.body.title) {
+    res.json("Request must at least contain a title for the note");
+    return;
+  }
+  let rawdata = fs.readFileSync('./db/db.json');
+  let currentNotes = JSON.parse(rawdata);
+  
+  //creates a new note from the body
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuid(),
+  }
+
+  //pushes the newNote
+  currentNotes.push(newNote)
+  //logs the currentNotes after the new note has been pushed
+  console.log(currentNotes);
+
+  //stringify the current notes to write in the database
+  var dataToSave = JSON.stringify(currentNotes)
+  fs.writeFileSync('./db/db.json', dataToSave);
+  res.json("New Note written!")
+});
+
+notes.delete("/:id",(req,res)=> {
+    const idToDelete = req.params.id
+
+    let rawdata = fs.readFileSync('./db/db.json');
+    let currentNotes = JSON.parse(rawdata);
+    console.info(`${req.method} request received`)
+
+//deletes the note from the database
+    currentNotes.splice(idToDelete)
+
+    var dataToSave = JSON.stringify(currentNotes)
+    fs.writeFileSync('./db/db.json', dataToSave);
+    res.json("Note deleted!")
+})
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
-// const {nanoid} = require("nanoid")
+
+//import router
+const api = require('./routes/index')
+
 const app = express();
 const PORT = 3001;
 
@@ -17,45 +19,7 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("/api/notes", (req, res) => {
-    let rawdata = fs.readFileSync('./db/db.json');
-    let currentNotes = JSON.parse(rawdata);
-    res.json(currentNotes)});
-
-app.post("/api/notes", (req, res) => {
-  console.info(`${req.method} request received`);
-  console.log(req.body)
-  if (!req.body.title) {
-    res.json("Request must at least contain a title for the note");
-    return;
-  }
-  let rawdata = fs.readFileSync('./db/db.json');
-  let currentNotes = JSON.parse(rawdata);
-  console.log(currentNotes);
-//   console.log(nanoid())
-  const newNote = {
-    title: req.body.title,
-    text: req.body.text,
-    id: "41234"
-  }
-  currentNotes.push(newNote)
-  console.log(currentNotes);
-  var dataToSave = JSON.stringify(currentNotes)
-  fs.writeFileSync('./db/db.json', dataToSave);
-  res.json("New Note written!")
-});
-
-// app.delete("/api/notes/:id",(req,res)=> {
-//     const idToDelete = req.params.id
-//     let rawdata = fs.readFileSync('./db/db.json');
-//     let currentNotes = JSON.parse(rawdata);
-// //manipulate the array accordingly
-
-// //be sure to stringify the data correctly once you edit what you'll write
-//     var dataToSave = JSON.stringify(currentNotes)
-//     fs.writeFileSync('./db/db.json', dataToSave);
-//     res.json("New Note written!")
-// })
+app.use('/api',api)
 
 app.listen(PORT, () => {
   console.log(`application succesfully listening to http://localhost:${PORT}`);
